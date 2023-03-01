3b6fcd36401182b424da4e2917a7572ace55637c
Nikolai <nikolai.stoiko@yandex.ru>
2023-03-01 16:30:45 +0300

    Realise context_manager

--- project/application/context_manager.py
+++ project/application/context_manager.py
@@ -1,10 +1,17 @@
+import os
+
 from project.utils.metaclasses import Singleton
 
 
 class ContextManager(metaclass=Singleton):
 
+    def __init__(self):
+        self.var_map: dict[str, str] = {}
+
     def get_var(self, name_str) -> str:
-        pass
+        if name_str in self.var_map.keys():
+            return self.var_map[name_str]
+        return os.environ.get(name_str, '')
 
     def set_var(self, name_str, value_str) -> None:
-        pass
+        self.var_map[name_str] = value_str
