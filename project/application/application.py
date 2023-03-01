0d3d920c631d3f2988405ec00d32f3e9f85e901a
RazerFord <desk10567@gmail.com>
2023-03-01 17:27:20 +0300

    Начал делать класс Application

--- project/application/application.py
+++ project/application/application.py
@@ -1,8 +1,32 @@
+from context_manager import ContextManager
+from project.parsing.constructor import Constructor
+from project.parsing.lexer import Lexer
+from project.parsing.substituter import Substituter
+from project.utils.metaclasses import Singleton
 
 
-class Application:
+class Application(metaclass=Singleton):
+    def __init__(self) -> None:
+        self.context_manager = ContextManager()
+
     def run(self):
-        pass
+        while True:
+            try:
+                stdin = input()
+                substitution_str = Substituter.substitute(stdin)
+                tokens = Lexer.lex(substitution_str)
+                executors = Constructor.construct(tokens)
+
+                out = None
+                for executor in executors:
+                    out = executor.exec(out)
+
+            except Exception as e:
+                print(type(e))
+                break
 
     def get_context_manager(self):
-        pass
+        return self.context_manager
+
+
+Application().run()
