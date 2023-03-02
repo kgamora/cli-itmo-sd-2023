122b4ac935b06b0a99d859caa61d3b50f37dae84
RazerFord <desk10567@gmail.com>
2023-03-03 00:33:01 +0300

    Реализовал Executor

---
+++ project/execution/exceptions/exception_exec.py
@@ -0,0 +1,3 @@
+from collections import namedtuple
+
+ExceptionExec = namedtuple("ExceptionExec", "stdout stderr code")
--- project/execution/executor.py
+++ project/execution/executor.py
@@ -1,6 +1,16 @@
+from project.execution.exceptions.exception_exec import ExceptionExec
 from project.execution.executable import Executable
 
 
 class Executor:
-    def exec(self, executables: list[Executable]):
-        pass
+    @staticmethod
+    def exec(executables: list[Executable]) -> ExceptionExec:
+        stdout, stderr, ret_code = None
+        for executable in executables:
+            executable.execute(stdout)
+            stdout = executable.stdout
+            stderr = executable.stderr
+            ret_code = executable.ret_code
+            if ret_code != 0:
+                break
+        return ExceptionExec(stdout, stderr, ret_code)
