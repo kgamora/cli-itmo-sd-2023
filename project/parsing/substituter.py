1944842ba205b0d9be85dc20e9d72f1210cbc0fe
Nikolai <nikolai.stoiko@yandex.ru>
2023-03-03 02:50:41 +0300

    Add Substitution of "" and ''

--- project/parsing/substituter.py
+++ project/parsing/substituter.py
@@ -4,7 +4,18 @@ class Substituter:
     @staticmethod
     def substitute(tokens_str: str) -> str:
         # passing without substitute at part1 of project
-        return tokens_str
+
+        result = tokens_str
+        try:
+            result = re.sub(r'"(.*)"', r'\1', tokens_str)
+        except Exception as e:
+            None
+        try:
+            result = re.sub(r'"(.*)"', r'\1', tokens_str)
+        except Exception as e:
+            None
+
+        return result
 
     @staticmethod
     def substitute_all(tokens_strings: list[str]) -> str:
