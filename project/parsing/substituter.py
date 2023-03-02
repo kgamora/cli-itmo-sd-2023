cc79da377c009cac9b15c74d2a225f342df4aa23
Nikolai <nikolai.stoiko@yandex.ru>
2023-03-03 00:43:01 +0300

    Add substitute_all

--- project/parsing/substituter.py
+++ project/parsing/substituter.py
@@ -5,3 +5,9 @@ class Substituter:
     def substitute(tokens_str: str) -> str:
         # passing without substitute at part1 of project
         return tokens_str
+
+    @staticmethod
+    def substitute_all(tokens_strings: list[str]) -> str:
+        # passing without substitute at part1 of project
+        for tok in tokens_strings:
+            yield Substituter.substitute(tok)
