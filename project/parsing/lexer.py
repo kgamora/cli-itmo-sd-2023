6f4c4b15f7d0769a5f70c73039d0af4c888b391c
Nikolai <nikolai.stoiko@yandex.ru>
2023-03-04 18:57:39 +0300

    Add unit-tests for parsing

--- project/parsing/lexer.py
+++ project/parsing/lexer.py
@@ -1,5 +1,8 @@
 import shlex
-from project.parsing.exceptions.LexingException import LexerException
+from project.parsing.exceptions.LexingException import (
+    LexerException,
+    LexerAssignException,
+)
 
 
 class Lexer:
@@ -11,7 +14,36 @@ class Lexer:
         :return: list of tokens
         """
         try:
-            result = shlex.split(tokens_str, posix=False)
+            tokens = shlex.split(tokens_str, posix=False)
+
+            i = 0
+            while i < len(tokens):
+                if (
+                    tokens[i][0] == "'"
+                    or tokens[i][-1] == "'"
+                    or tokens[i][0] == '"'
+                    or tokens[i][-1] == '"'
+                ):
+                    i += 1
+                    continue
+
+                new_tokens = tokens[i].split("=")
+                if len(new_tokens) > 2:
+                    raise LexerAssignException()
+                if (
+                    len(new_tokens) > 1
+                    and len(new_tokens[0]) > 0
+                    and len(new_tokens[1]) > 0
+                ):
+                    tokens = (
+                        tokens[0:i]
+                        + [new_tokens[0]]
+                        + ["="]
+                        + [new_tokens[1]]
+                        + tokens[i + 1 :]
+                    )
+                i += 1
+
         except ValueError as error:
             raise LexerException(error)
-        return result
+        return tokens
