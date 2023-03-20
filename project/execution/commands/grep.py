0a4a2e4d31c793fba9e1607d021a0c1ee430420b
Nikolai <nikolai.stoiko@yandex.ru>
2023-03-20 22:03:10 +0300

    Добавлена обработка ошибок regex : re.err

--- project/execution/commands/grep.py
+++ project/execution/commands/grep.py
@@ -5,40 +5,45 @@ from os.path import isfile
 
 
 class Grep(Executable):
+    class GrepException(Exception):
+        pass
+
+    _parser = CustomArgumentParser()
+    _parser.add_argument(
+        "-w", action="store_const", default=None, const=True, help="searching words"
+    )
+    _parser.add_argument(
+        "-A", default=0, type=int, help="how much lines show after matching"
+    )
+    _parser.add_argument(
+        "-i", action="store_const", default=None, const=True, help="ignore case"
+    )
+    _parser.add_argument(
+        "pattern", metavar="PATTERN", type=str, help="pattern to searching"
+    )
+    _parser.add_argument(
+        "files",
+        metavar="FILES",
+        type=str,
+        nargs="+",
+        help="list of files to search at",
+    )
+    _parser.prog = "grep"
+    _parser.description = """grep  searches  for  PATTERNS  in  each  FILE.  PATTERNS is one or more
+            patterns separated by newline characters, and  grep  prints  each  line
+            that  matches a pattern.
+            Typically, PATTERNS should be quoted when grep is used in a shell command."""
+
     def __init__(self, arguments: list[str] | None = None):
         super().__init__(arguments)
         self.os_asked_help = False
         try:
-            self.args = self._init_args()
+            self.args = self._parser.parse_args(self.arguments)
         except SystemExit as se:
             self.ret_code = se.code
         except CustomArgumentParser.ArgumentException as ex:
             self.stderr += str(ex) + "\n"
             self.ret_code = 2
-
-    def _init_args(self):
-        parser = CustomArgumentParser()
-        parser.add_argument(
-            "-w", action="store_const", default=None, const=True, help="searching words"
-        )
-        parser.add_argument(
-            "-A", default=0, type=int, help="how much lines show after matching"
-        )
-        parser.add_argument(
-            "-i", action="store_const", default=None, const=True, help="ignore case"
-        )
-        parser.add_argument(
-            "pattern", metavar="PATTERN", type=str, help="pattern to searching"
-        )
-        parser.add_argument(
-            "files",
-            metavar="FILES",
-            type=str,
-            nargs="+",
-            help="list of files to search at",
-        )
-        parser.prog = "grep"
-        return parser.parse_args(self.arguments)
 
     def _init_pattern(self):
         pattern: str
@@ -65,26 +70,35 @@ class Grep(Executable):
         result = list()
         current_accept = 0
 
-        for line in target_lines:
-            matches = re.search(pattern=pattern, string=line)
-            if matches:
-                current_accept = additional_lines_number
-                left, right = matches.span()
-                result.append((line, (left, right)))
-            elif current_accept > 0:
-                result.append((line, (0, 0)))
-                current_accept -= 1
-        return result
+        try:
+            for line in target_lines:
+                matches = re.search(pattern=pattern, string=line)
+                if matches:
+                    current_accept = additional_lines_number
+                    left, right = matches.span()
+                    result.append((line, (left, right)))
+                elif current_accept > 0:
+                    result.append((line, (0, 0)))
+                    current_accept -= 1
+            return result
+        except re.error as err:
+            raise Grep.GrepException(
+                f"{err.lineno}:{err.colno} Error of pattern matching: {err.msg}: '{err.pattern}'."
+            )
+        except BaseException:
+            raise Grep.GrepException("Error due pattern matching was occurred.")
 
     @Executable._may_throw
     def execute(self, stdin: str):
         """
-        Executes the command and captures its output (stdout, stderr and return code).
-        Behaves in the exact same manner as echo command in standard Linux distros but lacks its flags.
-        :param stdin: command input stream
+        grep  searches  for  PATTERNS  in  each  FILE.  PATTERNS is one or more
+        patterns separated by newline characters, and  grep  prints  each  line
+        that  matches a pattern.
+        Typically, PATTERNS should be quoted when grep is used in a shell command.
+        :param stdin: command input stream is ignored
         :return: None
         """
-        if self.ret_code:
+        if self.ret_code is not None:
             return
 
         pattern = self._init_pattern()
@@ -108,4 +122,3 @@ class Grep(Executable):
         with open(file_name, "r") as content_file:
             for line in content_file:
                 yield line
-        return
